c5cb77ee464015112ed21a4459b304ab16b785f7
Маша <makrosensei@gmail.com>
2020-06-02 19:05:21 +0300

    Все исправила

--- js/stat.js
+++ js/stat.js
@@ -7,7 +7,7 @@ var CLOUD_Y = 10;
 var SHADOW_GAP = 10;
 var EXTERNAL_GAP = 30;
 var FONT_GAP = 18;
-var GAP = 80;
+var COLUMN_GAP = 80;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var MAX_BAR_HEIGHT = 150;
@@ -29,7 +29,7 @@ var getMaxElement = function (arr) {
   return Math.max.apply(null, arr);
 };
 
-var getColumnColor = function () {
+var getBlueColor = function () {
   var randomNumber = (Math.floor(Math.random() * 100) + 1);
   return 'hsl(240,' + randomNumber + '%, 50%)';
 };
@@ -44,13 +44,12 @@ var drawHistogram = function (ctx, players, times) {
   players.forEach(function (player, i) {
     var barHeight = (MAX_BAR_HEIGHT * times[i]) / maxTime;
     var columnX = CLOUD_X + EXTERNAL_GAP + (BAR_GAP + BAR_WIDTH) * i;
-    var columnY = CLOUD_Y + GAP + (MAX_BAR_HEIGHT - barHeight);
-    var columnTextY = CLOUD_Y + GAP + MAX_BAR_HEIGHT + FONT_GAP;
+    var columnY = CLOUD_Y + COLUMN_GAP + (MAX_BAR_HEIGHT - barHeight);
+    var columnTextY = CLOUD_Y + COLUMN_GAP + MAX_BAR_HEIGHT + FONT_GAP;
 
     ctx.fillText(Math.ceil(times[i]), columnX, columnY - 10);
 
-    // теперь вроде работает только ESLint возмущается
-    player === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = getColumnColor();
+    ctx.fillStyle = player === 'Вы' ? 'rgba(255, 0, 0, 1)' : getBlueColor();
 
     drawColumn(ctx, columnX, columnY, BAR_WIDTH, barHeight);
 
